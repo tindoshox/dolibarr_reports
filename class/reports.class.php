5c08aa10e260e82fe5cc2df244fcb026530fe7c6
tindoshox <61076796+tindoshox@users.noreply.github.com>
2024-06-23 19:46:56 +0200

    Update reports.class.php
    Read database settings from Dolibarr global variables

--- class/reports.class.php
+++ class/reports.class.php
@@ -9,7 +9,8 @@ Class ReportGen {
 
 static public function get_report($params, $reportid, $passkey, $format=['pdf'])
 {
-	
+	global $dolibarr_main_db_name, $dolibarr_main_db_host, $dolibarr_main_db_user, $dolibarr_main_db_port, $dolibarr_main_db_pass, $dolibarr_main_db_type;
+
 	if (!file_exists(DOL_DATA_ROOT . '/reports/' . $reportid)) {
 		if (!mkdir($concurrentDirectory = DOL_DATA_ROOT . '/reports/' . $reportid, 0777, true) && !is_dir($concurrentDirectory)) {
 			throw new \RuntimeException(sprintf('Directory "%s" was not created', $concurrentDirectory));
@@ -36,12 +37,12 @@ static public function get_report($params, $reportid, $passkey, $format=['pdf'])
 		'locale' => 'en',
 		'params' => $params,
 		'db_connection' => [
-			'driver' => 'postgres',
-			'username' => $passkey,
-			'password' => $passkey,
-			'host' => '127.0.0.1',
-			'database' => $passkey,
-			'port' => '5432'
+			'driver' => $dolibarr_main_db_type === 'pgsql'?'postgres':'mysql',
+			'username' => $dolibarr_main_db_user,
+			'password' => $dolibarr_main_db_pass,
+			'host' => $dolibarr_main_db_host,
+			'database' => $dolibarr_main_db_name,
+			'port' => $dolibarr_main_db_port
 		]
 	];
 
